2cf655cc7ae5a93d605c50ae89f23c2880eab69a
Arturo Mendoza <91037768+dizsel74@users.noreply.github.com>
2023-07-03 18:41:14 -0600

    Task list inicio

---
+++ src/commponents/TaskList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function TaslList(props){
+  return(
+  <div className='task-list-container'>
+    <ul className='task-list'>
+      <ol className='task-item'></ol>
+    </ul>
+  </div>
+
+  );
+}
+
+export default TaskList;
+
+/*
+Lunes y Viernes de 3 a 7pm
+Martes a Jueves de 10 a 6pm
+Sábados de 10 am a 3pm
+*/
