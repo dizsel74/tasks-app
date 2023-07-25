36135d282b0192661debb792b7b9d06bdce12273
Arturo Mendoza <91037768+dizsel74@users.noreply.github.com>
2023-07-25 12:21:29 -0600

    UUID added

--- src/commponents/TaskForm.jsx
+++ src/commponents/TaskForm.jsx
@@ -6,22 +6,24 @@ function TaskForm(props){
 const [ input, setInput ] = useState('');
 
 const manejarCambios =  e => {
-  setInput (e.target.value);
+  setInput(e.target.value);
   console.log(e.target.value);
-
 }
 
 const manejarEnvio = e  =>{
+  e.preventDefault(); //evita que se recargue la app
+
   const tareaNueva ={
     id:'123',
-    texto:'ggg'
+    texto:input,
+    estado: false
   }
 }
 
   return(
-   <form className='task-form'>
+   <form className='task-form' onSubmit={manejarEnvio}>
     <input className='task-input' type="text" placeholder='add a task' name='texto' onChange={manejarCambios}/>
-    <button className='task-btn' onClick={ () => manejarEnvio}><AiOutlinePlus /></button>
+    <button className='task-btn'><AiOutlinePlus /></button>
    </form>
    
   );
